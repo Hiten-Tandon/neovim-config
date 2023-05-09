5c839c1bcf0eb14cac9a0aa38e065481fb14ff64
ProfessorNumbSkull <9660486666mtmtmt@gmail.com>
2023-05-09 11:23:38 +0530

    removed terminal plugin and added my own shortcut to open a terminal

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,55 +1,34 @@
--- You can add your own plugins here or in other files in this directory!
---  I promise not to create any merge conflicts in this directory :)
---
--- See the kickstart.nvim README for more information
 return {
 	{ "ErichDonGubler/lsp_lines.nvim" },
 	{
 		"nvim-tree/nvim-web-devicons",
-		config = function()
-			require 'nvim-web-devicons'.setup {
-				-- your personnal icons can go here (to override)
-				-- you can specify color or cterm_color instead of specifying both of them
-				-- DevIcon will be appended to `name`
-				override = {
-					zsh = {
-						icon = "",
-						color = "#428850",
-						cterm_color = "65",
-						name = "Zsh"
-					}
-				},
-				-- globally enable different highlight colors per icon (default to true)
-				-- if set to false all icons will have the default icon's color
-				color_icons = true,
-				-- globally enable default icons (default to false)
-				-- will get overriden by `get_icons` option
-				default = true,
-				-- globally enable "strict" selection of icons - icon will be looked up in
-				-- different tables, first by filename, and if not found by extension; this
-				-- prevents cases when file doesn't have any extension but still gets some icon
-				-- because its name happened to match some extension (default to false)
-				strict = true,
-				-- same as `override` but specifically for overrides by filename
-				-- takes effect when `strict` is true
-				override_by_filename = {
-					[".gitignore"] = {
-						icon = "",
-						color = "#f1502f",
-						name = "Gitignore"
-					}
-				},
-				-- same as `override` but specifically for overrides by extension
-				-- takes effect when `strict` is true
-				override_by_extension = {
-					["log"] = {
-						icon = '',
-						color = "#81e043",
-						name = "Log"
-					}
-				},
-			}
-		end
+		opts = {
+			override = {
+				zsh = {
+					icon = "",
+					color = "#428850",
+					cterm_color = "65",
+					name = "Zsh"
+				}
+			},
+			color_icons = true,
+			default = true,
+			strict = true,
+			override_by_filename = {
+				[".gitignore"] = {
+					icon = "",
+					color = "#f1502f",
+					name = "Gitignore"
+				}
+			},
+			override_by_extension = {
+				["log"] = {
+					icon = '',
+					color = "#81e043",
+					name = "Log"
+				}
+			},
+		}
 
 	},
 	{ "windwp/nvim-autopairs",        opts = {} },
