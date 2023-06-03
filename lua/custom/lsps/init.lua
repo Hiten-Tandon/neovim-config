45e945de75f64b5c2399b7fb2380341580f5a4e7
ProfessorNumbSkull <9660486666mtmtmt@gmail.com>
2023-06-03 08:24:58 +0530

    Fix: Added Pyright

--- lua/custom/lsps/init.lua
+++ lua/custom/lsps/init.lua
@@ -1,35 +1,36 @@
 return {
-	clangd = {}, -- C, C++
-	gopls = {}, -- Go
-	ruff_lsp = {}, -- Python
-	rust_analyzer = {
-		-- Rust
-		imports = {
-			granularity = {
-				group = 'module',
-			},
-			prefix = 'self',
-		},
-		procMacro = {
-			enable = true
-		},
-		checkOnSave = {
-			command = 'clippy'
-		},
-		dependencies = { 'simrat39\rust-tools.nvim' }
-	},
-	tsserver = {}, --TS, JS, HTML, ...
+  clangd = {},   -- C, C++
+  gopls = {},    -- Go
+  ruff_lsp = {}, -- Python
+  pyright = {},  -- Python
+  rust_analyzer = {
+    -- Rust
+    imports = {
+      granularity = {
+        group = 'module',
+      },
+      prefix = 'self',
+    },
+    procMacro = {
+      enable = true
+    },
+    checkOnSave = {
+      command = 'clippy'
+    },
+    dependencies = { 'simrat39\rust-tools.nvim' }
+  },
+  tsserver = {}, --TS, JS, HTML, ...
 
-	lua_ls = {
-		-- lua
-		Lua = {
-			workspace = { checkThirdParty = false },
-			telemetry = { enable = false },
-		},
-	},
+  lua_ls = {
+    -- lua
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
 
-	zls = {},                                                                       --zig
-	marksman = { dependencies = { 'davidgranstrom/nvim-markdown-preview', lazy = true }, } --markdown
+  zls = {},                                                                              --zig
+  marksman = { dependencies = { 'davidgranstrom/nvim-markdown-preview', lazy = true }, } --markdown
 }
 
 -- vim: ts=2 sts=2 sw=2 et
