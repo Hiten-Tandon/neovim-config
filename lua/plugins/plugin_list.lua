846c111aa3d47e1e1ab80525ee112076b110f8b8
Hiten Tandon <tandonhiten@gamil.com>
2024-10-22 05:44:59 +0530

    Added a key press plugin

--- lua/plugins/plugin_list.lua
+++ lua/plugins/plugin_list.lua
@@ -1,4 +1,10 @@
 return {
   { "williamboman/mason-lspconfig.nvim", enabled = false },
   { "williamboman/mason.nvim",           enabled = false },
+  { "OXY2DEV/markview.nvim" },
+  {
+    "NStefan002/screenkey.nvim",
+    lazy = false,
+    version = "*", -- or branch = "dev", to use the latest commit
+  }
 }
