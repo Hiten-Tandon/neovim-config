783bd5ef884862c13983d17f1a6c01f0810d2ac5
Hiten Tandon <tandonhiten@gamil.com>
2024-10-25 04:49:39 +0530

    Feat: Added MORE plugins

--- lua/plugins/plugin_list.lua
+++ lua/plugins/plugin_list.lua
@@ -6,5 +6,21 @@ return {
     "NStefan002/screenkey.nvim",
     lazy = false,
     version = "*", -- or branch = "dev", to use the latest commit
-  }
+  },
+  {
+    "mistricky/codesnap.nvim",
+    build = "make build_generator",
+    opts = {
+      watermark = "Hiten Tandon",
+      title = "Code By Hiten Tandon",
+      has_line_number = true,
+      code_font_family = "JetBrains Mono Nerd Font",
+      watermark_font_family = "JetBrains Mono Nerd Font",
+    },
+  },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
 }
