9ed8f7732ce2c8b28e1c62c35174915172dab24e
Hiten Tandon <tandonhiten@gamil.com>
2024-10-26 07:28:52 +0530

    Fix: REMOVED MASON

--- lua/plugins/plugin_list.lua
+++ lua/plugins/plugin_list.lua
@@ -1,12 +1,13 @@
 return {
   { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim",           enabled = false },
+  { "williamboman/mason.nvim", enabled = false },
   { "OXY2DEV/markview.nvim" },
   {
     "NStefan002/screenkey.nvim",
     lazy = false,
     version = "*", -- or branch = "dev", to use the latest commit
   },
+  { "brenoprata10/nvim-highlight-colors", opts = {} },
   {
     "mistricky/codesnap.nvim",
     build = "make build_generator",
