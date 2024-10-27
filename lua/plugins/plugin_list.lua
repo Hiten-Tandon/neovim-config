fcb6b64fa9e6d8f6d552ac118c0c64ca364bd8ab
Hiten Tandon <tandonhiten@gamil.com>
2024-10-28 00:52:18 +0530

    Feat: stuff

--- lua/plugins/plugin_list.lua
+++ lua/plugins/plugin_list.lua
@@ -1,13 +1,12 @@
 return {
   { "williamboman/mason-lspconfig.nvim", enabled = false },
-  { "williamboman/mason.nvim",           enabled = false },
   { "OXY2DEV/markview.nvim" },
   {
     "NStefan002/screenkey.nvim",
     lazy = false,
     version = "*", -- or branch = "dev", to use the latest commit
   },
-  { "sigmaSd/deno-nvim",                  opts = {} },
+  { "sigmaSd/deno-nvim", opts = {} },
   { "brenoprata10/nvim-highlight-colors", opts = {} },
   {
     "mistricky/codesnap.nvim",
