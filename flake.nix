a6986e5adaa77e861550c2d9c1591a7652a0c787
Hiten Tandon <tandonhiten@gamil.com>
2025-05-11 17:20:35 +0530

    Feat: More stuff

--- flake.nix
+++ flake.nix
@@ -3,33 +3,15 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     neovim-nightly.url = "github:nix-community/neovim-nightly-overlay";
     flake-utils.url = "github:numtide/flake-utils";
-    screen-key = {
-      url = "github:NStefan002/screenkey.nvim";
-      flake = false;
-    };
-    rose-pine = {
-      url = "github:rose-pine/neovim";
-      flake = false;
-    };
-    noice = {
-      url = "github:folke/noice.nvim";
-      flake = false;
-    };
-    nui = {
-      url = "github:MunifTanjim/nui.nvim";
-      flake = false;
-    };
-    which-key-nvim = {
-      url = "github:folke/which-key.nvim";
-      flake = false;
-    };
+    plugins.url = ./plugins;
   };
 
   outputs =
-    inputs@{
+    {
       nixpkgs,
       neovim-nightly,
       flake-utils,
+      plugins,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -38,33 +20,7 @@
         inherit system;
         overlays = [ neovim-nightly.overlays.default ];
         config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [ "codeium" ];
-      };
-      let
-        screen-key = vimUtils.buildVimPlugin {
-          name = "screen-key";
-          src = inputs.screen-key;
-        };
-        rose-pine = vimUtils.buildVimPlugin {
-          name = "rose-pine";
-          src = inputs.rose-pine;
-        };
-        nui = vimUtils.buildVimPlugin {
-          name = "nui";
-          src = inputs.nui;
-        };
-        noice = vimUtils.buildVimPlugin {
-          name = "noice";
-          src = inputs.noice;
-          dependencies = [ nui ];
-          nvimRequireCheck = "noice";
-        };
-        which-key-nvim = vimUtils.buildVimPlugin {
-          name = "which-key";
-          src = inputs.which-key-nvim;
-          nvimSkipModules = [ "which-key.docs" ];
-        };
-      in
-      {
+      }; {
         packages.default = wrapNeovimUnstable neovim-unwrapped {
           viAlias = true;
           vimAlias = true;
@@ -74,35 +30,32 @@
           withPerl = true;
           neovimRcContent = builtins.readFile ./init.vim;
           luaRcContent = builtins.readFile ./init.lua;
-          plugins = with vimPlugins; [
-            rose-pine
-            which-key-nvim
-            cord-nvim
-            screen-key
-            codesnap-nvim
-            snacks-nvim
-            lazy-nvim
-            nvim-web-devicons
-            noice
-            lazygit-nvim
-            nvim-treesitter
-            nvim-treesitter-textsubjects
-            nvim-treesitter-textobjects
-            nvim-treesitter-sexp
-            nvim-treesitter-refactor
-            nvim-treesitter-pyfold
-            nvim-treesitter-parsers.nix
-            nvim-lspconfig
-            lualine-nvim
-            windsurf-nvim
-            mini-icons
-            gitsigns-nvim
-            mini-diff
-            vim-nix
-            vim-addon-nix
-          ];
+          plugins =
+            (builtins.attrValues plugins.outputs.packages.${system})
+            ++ (with vimPlugins; [
+              # cord-nvim
+              snacks-nvim
+              lazy-nvim
+              nvim-web-devicons
+              lazygit-nvim
+              nvim-treesitter
+              nvim-treesitter-textsubjects
+              nvim-treesitter-textobjects
+              nvim-treesitter-sexp
+              nvim-treesitter-refactor
+              nvim-treesitter-pyfold
+              nvim-treesitter-parsers.nix
+              nvim-lspconfig
+              lualine-nvim
+              windsurf-nvim
+              mini-icons
+              gitsigns-nvim
+              mini-diff
+              vim-nix
+              vim-addon-nix
+            ]);
         };
-        formatter = nixfmt-rfc-style;
+        formatter = nixfmt-tree;
       }
     );
 }
---
+++ plugins/flake.nix
@@ -0,0 +1,152 @@
+{
+  inputs = {
+    codesnap-nvim = {
+      url = "github:mistricky/codesnap.nvim";
+      flake = false;
+    };
+    screen-key = {
+      url = "github:NStefan002/screenkey.nvim";
+      flake = false;
+    };
+    rose-pine = {
+      url = "github:rose-pine/neovim";
+      flake = false;
+    };
+    noice = {
+      url = "github:folke/noice.nvim";
+      flake = false;
+    };
+    nui = {
+      url = "github:MunifTanjim/nui.nvim";
+      flake = false;
+    };
+    which-key-nvim = {
+      url = "github:folke/which-key.nvim";
+      flake = false;
+    };
+    cord-nvim = {
+      url = "github:vyfor/cord.nvim";
+      flake = false;
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    inputs@{ nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      with import nixpkgs { inherit system;};
+      let
+        basePlugins = builtins.mapAttrs (_: v: vimUtils.buildVimPlugin v) (
+          builtins.mapAttrs (name: src: { inherit name src; }) (
+            builtins.removeAttrs inputs [
+              "nixpkgs"
+              "self"
+              "flake-utils"
+              "rust-overlay"
+            ]
+          )
+        );
+        plugins = basePlugins // {
+          noice = basePlugins.noice.overrideAttrs {
+            dependencies = [ basePlugins.nui ];
+            nvimRequireCheck = "noice";
+          };
+          which-key-nvim = basePlugins.which-key-nvim.overrideAttrs {
+            nvimSkipModules = [ "which-key.docs" ];
+          };
+          cord-nvim =
+            let
+              version = "nightly";
+              src = inputs.cord-nvim;
+              rustPackage = rustPlatform.buildRustPackage {
+                pname = "cord.nvim-rust";
+                inherit version src;
+                RUSTC_BOOTSTRAP = true;
+
+                cargoHash = "sha256-5BsJJDQC/6VqkBIQf5SwBN5n6Ow3Hupr0Gfda5hG5WU=";
+
+                installPhase =
+                  let
+                    cargoTarget = stdenv.hostPlatform.rust.cargoShortTarget;
+                  in
+                  ''
+                    install -D target/${cargoTarget}/release/cord $out/lib/cord
+                  '';
+              };
+            in
+            vimUtils.buildVimPlugin {
+              pname = "cord.nvim";
+              inherit version src;
+
+              nativeBuildInputs = [
+                rustPackage
+              ];
+
+              buildPhase = ''
+                install -D ${rustPackage}/lib/cord cord
+              '';
+
+              installPhase = ''
+                install -D cord $out/lua/cord
+              '';
+
+              doInstallCheck = true;
+              nvimRequireCheck = "cord";
+
+              meta = {
+                homepage = "https://github.com/vyfor/cord.nvim";
+              };
+            };
+
+          codesnap-nvim =
+            let
+              version = "nightly";
+              src = runCommandNoCCLocal "writable-codesnap-src" { } ''
+                cp -r ${inputs.codesnap-nvim}/generator $out
+              '';
+              codesnap-lib = rustPlatform.buildRustPackage {
+                pname = "codesnap";
+                inherit version src;
+
+                cargoHash = "sha256-tg4BO4tPzHhJTowL7RiAuBo4i440FehpGmnz9stTxfI=";
+                nativeBuildInputs = [
+                  pkg-config
+                  rustPlatform.bindgenHook
+                ];
+
+                buildInputs =
+                  [
+                    libuv.dev
+                  ]
+                  ++ lib.optionals stdenv.hostPlatform.isDarwin [
+                    darwin.apple_sdk.frameworks.AppKit
+                  ];
+              };
+            in
+            vimUtils.buildVimPlugin {
+              pname = "codesnap.nvim";
+              inherit version;
+              src = inputs.codesnap-nvim;
+
+              postInstall =
+                let
+                  extension = if stdenv.hostPlatform.isDarwin then "dylib" else "so";
+                in
+                ''
+                  rm -rf $out/lua/*.so
+                  cp ${codesnap-lib}/lib/libgenerator.${extension} $out/lua/generator.so
+                '';
+
+              doInstallCheck = true;
+              nvimRequireCheck = "codesnap";
+            };
+        };
+      in
+      {
+        packages = plugins;
+        formatter = nixfmt-tree;
+      }
+    );
+}
