d4db95f3d135bf215e4caadcbf15d45f3bd383f7
Hiten Tandon <tandonhiten@gamil.com>
2025-05-20 04:56:20 +0530

    I think this works

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
       neovim-nightly,
       flake-utils,
       plugins,
+      self,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
@@ -20,18 +21,32 @@
         inherit system;
         overlays = [ neovim-nightly.overlays.default ];
         config.allowUnfreePredicate = pkg: builtins.elem (nixpkgs.lib.getName pkg) [ "codeium" ];
-      }; {
-        packages = rec {
+      }; rec {
+        languages = {
+          rust = {
+            lsp = rust-analyzer;
+            formatter = rustfmt;
+            programs = [ cargo ];
+            plugins = [ vimPlugins.rustaceanvim ];
+          };
+        };
+        packages = {
           default = symlinkJoin {
             name = "neovim";
-            paths = [
-              fd
-              neovim
-              tree-sitter
-              imagemagick
-              ghostscript
-              lua.withPackages(p: with p; [lua-lsp])
-            ];
+            paths =
+              ([
+                fd
+                packages.neovim
+                tree-sitter
+                imagemagick
+                ghostscript
+                (lua.withPackages (p: with p; [ lua-lsp ]))
+              ]
+              ++ languages.rust.programs
+              ++ (with languages.rust; [
+                lsp
+                formatter
+              ]));
             meta.mainProgram = "nvim";
           };
           neovim = (
@@ -46,17 +61,21 @@
               luaRcContent = builtins.readFile ./init.lua;
               plugins =
                 (builtins.attrValues plugins.outputs.packages.${system})
-                ++ (with vimPlugins; [
-                  nvim-treesitter
-                  nvim-treesitter-textsubjects
-                  nvim-treesitter-textobjects
-                  nvim-treesitter-sexp
-                  nvim-treesitter-refactor
-                  nvim-treesitter-pyfold
-                  nvim-treesitter-parsers.nix
-                  nvim-treesitter-parsers.lua
-                  windsurf-nvim
-                ]);
+                ++ (
+                  with vimPlugins;
+                  [
+                    nvim-treesitter
+                    nvim-treesitter-textsubjects
+                    nvim-treesitter-textobjects
+                    nvim-treesitter-sexp
+                    nvim-treesitter-refactor
+                    nvim-treesitter-pyfold
+                    nvim-treesitter-parsers.nix
+                    nvim-treesitter-parsers.lua
+                    windsurf-nvim
+                  ]
+                  ++ languages.rust.plugins
+                );
             }
           );
         };
